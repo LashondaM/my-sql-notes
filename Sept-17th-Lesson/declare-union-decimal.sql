19dc792993c9468cdf062a85bbb54df4fd5c8af2
LashondaM <67280594+LashondaM@users.noreply.github.com>
2021-09-20 09:38:56 -0400

    Update declare-union-decimal.sql

--- Sept-17th-Lesson/declare-union-decimal.sql
+++ Sept-17th-Lesson/declare-union-decimal.sql
@@ -1,4 +1,4 @@
-declare @upperGpa decimal(5,1) = 3.5; -- the 5 means you'll never see a number with 5 non 0 digits, and 1 is the of numbers following the decimal
+declare @upperGpa decimal(5,1) = 3.5; -- the 5 means you'll never see a number with 5 non 0 digits, and 1 is the amount of numbers following the decimal
 declare @lowerGpa decimal(5,1) = 2.5; -- the highest number this can go to is 9,999.9
 
 select *
